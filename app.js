46e32d5784a6567e2088dfa87331ea99b4f2b9b2
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2021-05-08 13:18:16 -0400

    added sounds functionality

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ class Drumkit {
         this.playBtn = document.querySelector(".play");
         this.kickAudio = document.querySelector(".kick-sound");
         this.snareAudio = document.querySelector(".snare-sound");
-        this.hihatAudio = document.querySelector(".hithat-sound");
+        this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
-        this.bpm = 150;
+        this.bpm = 200;
 
     }
     activePad() {
@@ -16,7 +16,26 @@ class Drumkit {
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
-        console.log(step);
+        //loop of the bars
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            //check if pads are active
+            if (bar.classList.contains("active")) {
+                //check each sounds
+                if (bar.classList.contains("kick-pad")) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains("snare-pad")) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains("hihat-pad")) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
 
@@ -32,7 +51,10 @@ class Drumkit {
 const drumkit = new Drumkit();
 
 drumkit.pads.forEach(pad => {
-    pad.addEventListener("click", drumkit.activePad)
+    pad.addEventListener("click", drumkit.activePad);
+    pad.addEventListener("animationend", function () {
+        this.style.animation = "";
+    });
 });
 
 drumkit.playBtn.addEventListener("click", function () {
